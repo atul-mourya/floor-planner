a693ad3b0e14cf13d96aa559d64fb7e3de4f62a3
atul.mourya@gmail.com <atul.mourya@gmail.com>
2022-08-20 11:18:31 +0530

    Updates

---
+++ precache-manifest.4722a26ae3279fa4969662508fec2097.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "55b2ecef47a48e971fce",
+    "url": "/floor-planner/static/css/main.ff44211d.chunk.css"
+  },
+  {
+    "revision": "55b2ecef47a48e971fce",
+    "url": "/floor-planner/static/js/main.55b2ecef.chunk.js"
+  },
+  {
+    "revision": "cadd0c8c2ad46ff4c350",
+    "url": "/floor-planner/static/js/1.cadd0c8c.chunk.js"
+  },
+  {
+    "revision": "cd7dc49f6f803b780c2d",
+    "url": "/floor-planner/static/js/runtime~main.cd7dc49f.js"
+  },
+  {
+    "revision": "bc86ec2980dac8d970a08beb21683916",
+    "url": "/floor-planner/index.html"
+  }
+];
